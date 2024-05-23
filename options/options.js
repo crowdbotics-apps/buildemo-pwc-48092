159bfa94690605db496dc2d30e7d4c8b4e6eb9bc
Crowdbotics <team@crowdbotics.com>
2024-05-23 22:22:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "buildemo-pwc-48092",
+  url: "https://buildemo-pwc-48092.botics.co",
+  api: "https://buildemo-pwc-48092.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
